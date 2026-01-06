ddbdfd85bbfe04cd81483d2c47005e6b22cf5386
nathezek <nathezek7@gmail.com>
2026-01-06 09:22:47 +0300

    chore: Removed vue bolier-plates

---
+++ server/target/debug/deps/server-7db2a309a98cd484.d
@@ -0,0 +1,5 @@
+/home/comrade/Documents/projects/hackhaton-projects/notice/server/target/debug/deps/server-7db2a309a98cd484.d: src/main.rs
+
+/home/comrade/Documents/projects/hackhaton-projects/notice/server/target/debug/deps/libserver-7db2a309a98cd484.rmeta: src/main.rs
+
+src/main.rs:

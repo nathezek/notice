d52a15d35b03d61217b861fd5bf84afe9d902eff
nathezek <nathezek7@gmail.com>
2026-01-06 10:08:49 +0300

    chore: Removed target makefile binaries

--- server/target/debug/deps/server-f21852bb710a2845.d
+++
@@ -1,5 +0,0 @@
-/home/comrade/Documents/projects/hackhaton-projects/notice/server/target/debug/deps/server-f21852bb710a2845.d: src/main.rs
-
-/home/comrade/Documents/projects/hackhaton-projects/notice/server/target/debug/deps/libserver-f21852bb710a2845.rmeta: src/main.rs
-
-src/main.rs:
